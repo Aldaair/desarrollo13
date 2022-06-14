677441068f3ad9f84c2eab3c4b8d4d84ed85f6cb
Aldaaairr <aldaair10@gmail.com>
2022-06-13 23:09:56 -0500

    asd

---
+++ database/migrations/2022_06_14_022156_create_libros_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLibrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('resumen');
+            $table->integer('npagina');
+            $table->integer('edicion');
+            $table->string('autor');
+            $table->decimal('precio',5,2);
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libros');
+    }
+}
+
